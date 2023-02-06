0191fefef710173540557d2f111c0f7e8a100b08
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-02-07 00:26:59 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,73 @@
+// Завдання1
+
+// let n=prompt('write n');
+// let result=1;
+// for(let i=1;i<=n;i++){
+//     result*=i;
+// }
+// alert(result)
+
+
+//Завдання 2
+
+// for(let i=1000;i<=9999;i+=3){
+//     document.write(` ${i} `)
+// }
+
+// Завдання 3
+
+    // let n=-1
+    // for(let i=0;i<=55;i++){
+    //     n+=2;
+    //     document.write(` ${n} `)
+    // }
+
+    //Завдання 4
+
+    // for(let i=90; i>=0;i=i-5){
+           
+    //         document.write(` ${i} `)
+    //     }  
+
+    // Завдання 5
+
+    // let n=1
+    // for(let i=1;i<=20;i++){
+        
+    //     n=2*n;
+    //     document.write(` ${n} `)
+    // }
+
+    // Завдання 6
+
+    // for(let a=2; a<=10000;a=2*a-1){
+    //     document.write(` ${a} `)
+    // }
+
+    // Завдання 7
+
+
+    // for(let a=-166; a<100; a=2*a+200){
+    // if(a>-9|| a>-100|| a>9){
+    //     document.write (` ${a} `)  }
+    // }
+    
+    // Завдання 8
+
+    // let n1 = +prompt("Введіть число");
+
+    // let n2 = +prompt("Введіть степень");
+    
+    // let result = 1;
+    // let check;
+
+    // if(0>n2){
+    //     check = -n2;
+    //     n2 = check;
+    // }
+    
+    // for(let i=1; i<=n2;i++){
+    //     result *=n1 ;
+    // }
+    
+    // alert(`Result = ${result}`);
